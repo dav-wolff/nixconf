739bf1b431e3fcbfbbb77b7bcb3d898e278ea54f
David Wolff <david@dav.dev>
2023-05-12 01:40:34 +0200

    Install rust

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,10 @@
   environment.systemPackages = with pkgs; [
     git
     helix
+    gcc
+    rustc
+    cargo
+    clippy
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
