7d9a805e7c9620f770a88ae94ffb6c13496ca5e2
David Wolff <david@dav.dev>
2024-05-30 18:45:20 +0200

    Imporve zsh templating

--- packages/zsh.nix
+++ packages/zsh.nix
@@ -3,25 +3,35 @@
 let
 	zsh = pkgs.zsh;
 	
+	templateReplacements = with pkgs; {
+		P10K = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
+		DEFER = "${zsh-defer}/share/zsh-defer";
+		SYNTAX_HIGHLIGHTING = "${zsh-syntax-highlighting}/share/zsh-syntax-highlighting";
+		COMPLETIONS = "${zsh-completions}/share/zsh/site-functions";
+		AUTOSUGGESTIONS = "${zsh-autosuggestions}/share/zsh-autosuggestions";
+		FZF = "${fzf}/bin";
+		ZOXIDE = "${zoxide}/bin";
+	};
+	
+	templateReplace = pkgs.lib.concatMapStrings (name:
+		let
+			value = builtins.getAttr name templateReplacements;
+		in ''s\@${name}@\${value}\g;''
+	) (builtins.attrNames templateReplacements);
+	
 	compiledConfig = pkgs.runCommand "${zsh.name}-config" {
 		pname = "${zsh.name}-config";
 		nativeBuildInputs = [zsh];
-	} (with pkgs; ''
+	} ''
 		mkdir $out
 		cp ${./zsh/zshrc.zsh} $out/.zshrc
 		cp ${./zsh/p10k_nerd_font.zsh} $out/p10k_nerd_font.zsh
 		cp ${./zsh/p10k_tty.zsh} $out/p10k_tty.zsh
-		sed -i "s\@P10K@\${zsh-powerlevel10k}/share/zsh-powerlevel10k\g" $out/.zshrc
-		sed -i "s\@DEFER@\${zsh-defer}/share/zsh-defer\g" $out/.zshrc
-		sed -i "s\@SYNTAX_HIGHLIGHTING@\${zsh-syntax-highlighting}/share/zsh-syntax-highlighting\g" $out/.zshrc
-		sed -i "s\@COMPLETIONS@\${zsh-completions}/share/zsh/site-functions\g" $out/.zshrc
-		sed -i "s\@AUTOSUGGESTIONS@\${zsh-autosuggestions}/share/zsh-autosuggestions\g" $out/.zshrc
-		sed -i "s\@FZF@\${fzf}/bin\g" $out/.zshrc
-		sed -i "s\@ZOXIDE@\${zoxide}/bin\g" $out/.zshrc
+		sed -i "${templateReplace}" $out/.zshrc
 		zsh -c "zcompile $out/.zshrc"
 		zsh -c "zcompile $out/p10k_nerd_font.zsh"
 		zsh -c "zcompile $out/p10k_tty.zsh"
-	'');
+	'';
 in
 	pkgs.runCommand zsh.name {
 		inherit (zsh) pname version meta;
