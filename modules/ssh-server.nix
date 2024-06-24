872163ea2fa72d99bfb8f757507e52b8c77607c7
David Wolff <david@dav.dev>
2024-06-25 01:04:23 +0200

    Add public key for sub

--- modules/ssh-server.nix
+++ modules/ssh-server.nix
@@ -7,5 +7,6 @@
 	
 	users.users.dav.openssh.authorizedKeys.keys = [
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIII7qaNDRTHgkgaPBsc2X7N7Aovw2s+uBhNAQLfYfqe4 david@dav.dev" # max
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1argtOi0LVxZDkfoWmc0/5uG6p4JIyFKEm8wRyG4Rs david@dav.dev" # sub
 	];
 }
