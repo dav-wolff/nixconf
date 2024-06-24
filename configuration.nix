8caa2f2a6fb4ef4277d9badaab8cd0bd290dcdba
David Wolff <david@dav.dev>
2024-06-25 01:27:46 +0200

    Add nix substituters

--- configuration.nix
+++ configuration.nix
@@ -5,10 +5,24 @@
 { self, pkgs, name, ndent, journal, ... }:
 
 {
-	nix.settings.experimental-features = [
-		"nix-command"
-		"flakes"
-	];
+	nix.settings = {
+		experimental-features = [
+			"nix-command"
+			"flakes"
+		];
+		
+		substituters = [
+			"https://cache.nixos.org"
+			"https://nix-community.cachix.org"
+			"https://helix.cachix.org"
+		];
+		
+		trusted-public-keys = [
+			"hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+			"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+			"helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+		];
+	};
 	
 	nixpkgs.config.allowUnfree = true;
 	
