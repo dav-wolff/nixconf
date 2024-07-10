995c0f9b22f1232f0f77aa6ceba17e012b9dee6a
David Wolff <david@dav.dev>
2024-07-10 05:00:29 +0200

    Add public keys for top

--- public-keys.nix
+++ public-keys.nix
@@ -1,12 +1,14 @@
 let
 	userKeys = {
 		max = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIII7qaNDRTHgkgaPBsc2X7N7Aovw2s+uBhNAQLfYfqe4 david@dav.dev";
+		top = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3VE3LCcpzIc8HKgZ1JZtheHJKsQ+QFjCrXB5/RuGBH david@dav.dev";
 		sub = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1argtOi0LVxZDkfoWmc0/5uG6p4JIyFKEm8wRyG4Rs david@dav.dev";
 		shuttle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcgcxUDXSuNmHkmBiVte8zIQCRAiw/LX4TA3/M0dvb9 dav@dav.dev";
 	};
 	
 	hostKeys = {
 		max = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbFDtbeU7fZREtiNRBaL0T8Ro599HO6t3h5NmzWNJ6Q root@max";
+		top = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/mtD2gnkt0pvRXKh7AUn7mPQ2TMqdaHLQfsPCDPhB6 root@top";
 		shuttle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdIp/YIm2ncVFMmEqtg7y4cbnzP1tjX2Jx/YXRJsT15 root@shuttle";
 	};
 in {
