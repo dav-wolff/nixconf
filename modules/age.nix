e650f4879a5e53a68c8c4e54da102d2632845345
David Wolff <david@dav.dev>
2024-07-15 22:19:47 +0200

    Use lib.mkMerge to properly merge uses of lib.mkIf

--- modules/age.nix
+++ modules/age.nix
@@ -1,37 +1,41 @@
 { config, lib, pkgs, ... }:
 
 {
-	config = {
-		environment.systemPackages = [
-			pkgs.agenix
-		];
-	} // lib.mkIf (config.age.secrets != {} && !config.services.openssh.enable) {
-		# If openssh is not enabled, it won't generate its keys.
-		# However, they're still required for agenix to work.
-		system.activationScripts.generateSSHKeys = {
-			# from nixpkgs/nixos/modules/services/networking/ssh/sshd.nix
-			text = with lib; flip concatMapStrings config.services.openssh.hostKeys (k: ''
-				if ! [ -s "${k.path}" ]; then
-						if ! [ -h "${k.path}" ]; then
-								rm -f "${k.path}"
-						fi
-						mkdir -m 0755 -p "$(dirname '${k.path}')"
-						${pkgs.openssh}/bin/ssh-keygen \
-							-t "${k.type}" \
-							${optionalString (k ? bits) "-b ${toString k.bits}"} \
-							${optionalString (k ? rounds) "-a ${toString k.rounds}"} \
-							${optionalString (k ? comment) "-C '${k.comment}'"} \
-							${optionalString (k ? openSSHFormat && k.openSSHFormat) "-o"} \
-							-f "${k.path}" \
-							-N ""
-				fi
-			'');
+	config = let
+		baseConfig = {
+			environment.systemPackages = [
+				pkgs.agenix
+			];
 		};
 		
-		system.activationScripts.agenixInstall.deps = ["generateSSHKeys"];
-		
-		# agenix only sets identityPaths by default if openssh is enabled
-		# from agenix/modules/age.nix
-		age.identityPaths = with lib; map (e: e.path) (filter (e: e.type == "rsa" || e.type == "ed25519") config.services.openssh.hostKeys);
-	};
+		disabledSshConfig = lib.mkIf (config.age.secrets != {} && !config.services.openssh.enable) {
+			# If openssh is not enabled, it won't generate its keys.
+			# However, they're still required for agenix to work.
+			system.activationScripts.generateSSHKeys = {
+				# from nixpkgs/nixos/modules/services/networking/ssh/sshd.nix
+				text = with lib; flip concatMapStrings config.services.openssh.hostKeys (k: ''
+					if ! [ -s "${k.path}" ]; then
+							if ! [ -h "${k.path}" ]; then
+									rm -f "${k.path}"
+							fi
+							mkdir -m 0755 -p "$(dirname '${k.path}')"
+							${pkgs.openssh}/bin/ssh-keygen \
+								-t "${k.type}" \
+								${optionalString (k ? bits) "-b ${toString k.bits}"} \
+								${optionalString (k ? rounds) "-a ${toString k.rounds}"} \
+								${optionalString (k ? comment) "-C '${k.comment}'"} \
+								${optionalString (k ? openSSHFormat && k.openSSHFormat) "-o"} \
+								-f "${k.path}" \
+								-N ""
+					fi
+				'');
+			};
+			
+			system.activationScripts.agenixInstall.deps = ["generateSSHKeys"];
+			
+			# agenix only sets identityPaths by default if openssh is enabled
+			# from agenix/modules/age.nix
+			age.identityPaths = with lib; map (e: e.path) (filter (e: e.type == "rsa" || e.type == "ed25519") config.services.openssh.hostKeys);
+		};
+	in lib.mkMerge [baseConfig disabledSshConfig];
 }
--- modules/web-server.nix
+++ modules/web-server.nix
@@ -48,6 +48,17 @@ in {
 				default = "${cfg.solitaire.subdomain}.${cfg.domain}";
 			};
 		};
+		
+		nextcloud = {
+			enable = lib.mkEnableOption "webServer.nextcloud";
+			subdomain = lib.mkOption {
+				type = lib.types.str;
+			};
+			domain = lib.mkOption {
+				type = lib.types.str;
+				default = "${cfg.nextcloud.subdomain}.${cfg.domain}";
+			};
+		};
 	};
 	
 	config = lib.mkIf cfg.enable {
@@ -59,16 +70,10 @@ in {
 		security.acme.certs.${cfg.domain}.extraDomainNames = ["www.${cfg.domain}"]
 			++ lib.optional cfg.vault.enable cfg.vault.domain
 			++ lib.optional cfg.bitwarden.enable cfg.bitwarden.domain
-			++ lib.optional cfg.solitaire.enable cfg.solitaire.domain;
+			++ lib.optional cfg.solitaire.enable cfg.solitaire.domain
+			++ lib.optional cfg.nextcloud.enable cfg.nextcloud.domain;
 		
-		services.nginx = let
-			locations."/" = {
-				return = "200 'Hello world'";
-				extraConfig = ''
-					add_header Content-Type text/plain;
-				'';
-			};
-		in {
+		services.nginx = {
 			enable = true;
 			recommendedProxySettings = true;
 			recommendedTlsSettings = true;
@@ -77,51 +82,77 @@ in {
 			recommendedZstdSettings = true;
 			recommendedBrotliSettings = true;
 			
-			virtualHosts = {
-				${cfg.domain} = {
-					forceSSL = true;
-					enableACME = true;
-					inherit locations;
+			virtualHosts = let
+				baseHosts = let
+					locations."/" = {
+						return = "200 'Hello world'";
+						extraConfig = ''
+							add_header Content-Type text/plain;
+						'';
+					};
+				in {
+					${cfg.domain} = {
+						forceSSL = true;
+						enableACME = true;
+						inherit locations;
+					};
+					
+					"www.${cfg.domain}" = {
+						forceSSL = true;
+						useACMEHost = cfg.domain;
+						inherit locations;
+					};
 				};
 				
-				"www.${cfg.domain}" = {
-					forceSSL = true;
-					useACMEHost = cfg.domain;
-					inherit locations;
-				};
-			} // lib.optionalAttrs cfg.vault.enable {
-				${cfg.vault.domain} = {
-					forceSSL = true;
-					useACMEHost = cfg.domain;
-					locations."/".proxyPass = "http://localhost:${toString cfg.vault.port}";
-				};
-			} // lib.optionalAttrs cfg.bitwarden.enable {
-				${cfg.bitwarden.domain} = {
-					forceSSL = true;
-					useACMEHost = cfg.domain;
-					locations."/" = {
-						proxyPass = "http://localhost:${toString cfg.bitwarden.port}";
-						proxyWebsockets = true;
+				vaultHosts = lib.mkIf cfg.vault.enable {
+					${cfg.vault.domain} = {
+						forceSSL = true;
+						useACMEHost = cfg.domain;
+						locations."/".proxyPass = "http://localhost:${toString cfg.vault.port}";
 					};
 				};
-			} // lib.optionalAttrs cfg.solitaire.enable {
-				${cfg.solitaire.domain} = {
-					forceSSL = true;
-					useACMEHost = cfg.domain;
-					locations."= /index.html" = {
-						root = pkgs.solitaire.web;
-						extraConfig = ''
-							add_header Cache-Control "public, no-cache";
-						'';
-					};
-					locations."/" = {
-						root = pkgs.solitaire.web;
-						extraConfig = ''
-							add_header Cache-Control "public, max-age=604800, immutable";
-						'';
+				
+				bitwardenHosts = lib.mkIf cfg.bitwarden.enable {
+					${cfg.bitwarden.domain} = {
+						forceSSL = true;
+						useACMEHost = cfg.domain;
+						locations."/" = {
+							proxyPass = "http://localhost:${toString cfg.bitwarden.port}";
+							proxyWebsockets = true;
+						};
 					};
 				};
-			};
+				
+				solitaireHosts = lib.mkIf cfg.solitaire.enable {
+					${cfg.solitaire.domain} = {
+						forceSSL = true;
+						useACMEHost = cfg.domain;
+						locations."= /index.html" = {
+							root = pkgs.solitaire.web;
+							extraConfig = ''
+								add_header Cache-Control "public, no-cache";
+							'';
+						};
+						locations."/" = {
+							root = pkgs.solitaire.web;
+							extraConfig = ''
+								add_header Cache-Control "public, max-age=604800, immutable";
+							'';
+						};
+					};
+				};
+				
+				nextcloudHosts = lib.mkIf cfg.nextcloud.enable {
+					${cfg.nextcloud.domain} = {
+						forceSSL = true;
+						useACMEHost = cfg.domain;
+						# All other options are managed by services.nextcloud
+					};
+				};
+			in lib.mkMerge [baseHosts vaultHosts bitwardenHosts solitaireHosts nextcloudHosts];
 		};
-	};
+		
+		services.nextcloud.hostName = lib.mkIf (cfg.enable && cfg.nextcloud.enable) cfg.nextcloud.domain;
+		# All other options are managed my modules.nextcloud
+	} ;
 }
