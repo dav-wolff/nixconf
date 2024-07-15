6ea4127bedffd265bf3f15144a07078a221e3a9c
David Wolff <david@dav.dev>
2024-07-15 17:55:31 +0200

    Update system.stateVersion to 24.05

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ pkgs,  ... }:
+{ pkgs, ... }:
 
 {
 	
@@ -80,5 +80,5 @@
 	# this value at the release version of the first install of this system.
 	# Before changing this value read the documentation for this option
 	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "22.11"; # Did you read the comment?
+	system.stateVersion = "24.05"; # Did you read the comment?
 }
