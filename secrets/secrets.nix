8ca4b18e2588294b75e376776f9afb172cbb741a
David Wolff <david@dav.dev>
2024-07-30 01:57:25 +0200

    Provide context for owntracks secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,6 @@ let
 in {
 	"hotspotPassword.age".publicKeys = [keys.hostKeys.shuttle];
 	"vaultwardenKey.age".publicKeys = [keys.hostKeys.shuttle];
-	"owntracksPassword.age".publicKeys = [keys.hostKeys.shuttle]; # 
+	"owntracksPassword.age".publicKeys = [keys.hostKeys.shuttle]; # htpasswd -c /path/to/owntracksPassword.age dav
 	"keys.gpg.age".publicKeys = keys.allHostKeys; # gpg --export-secret-keys --export-options backup --output keys.gpg
 }
