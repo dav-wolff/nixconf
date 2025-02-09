c24a80e8326cedf7e4e5de5e50326e62d1f57edb
David Wolff <david@dav.dev>
2025-02-09 22:04:50 +0100

    Require authentication for OpenSMTPD

--- modules/age.nix
+++ modules/age.nix
@@ -1,14 +1,66 @@
 { config, lib, pkgs, ... }:
 
-{
-	config = let
-		baseConfig = {
+let
+	cfg = config.age;
+in {
+	# extend agenix functionality
+	options.age = with lib; {
+		derivedSecrets = mkOption {
+			default = {};
+			type = types.attrsOf (types.submodule ({config, ...}: {
+				options = {
+					name = mkOption {
+						type = types.str;
+						default = config._module.args.name;
+					};
+					secret = mkOption {
+						type = types.str;
+					};
+					script = mkOption {
+						type = types.str;
+					};
+					inputs = mkOption {
+						type = types.listOf types.package;
+						default = [];
+					};
+					path = mkOption {
+						type = types.str;
+						default = "${cfg.secretsDir}/${config.name}";
+					};
+				};
+			}));
+		};
+	};
+	
+	config = lib.mkMerge [
+		{
 			environment.systemPackages = [
 				pkgs.agenix
 			];
-		};
+		}
 		
-		disabledSshConfig = lib.mkIf (config.age.secrets != {} && !config.services.openssh.enable) {
+		(lib.mkIf (cfg.derivedSecrets != {}) {
+			system.activationScripts.agenixDerivedSecrets = let
+				deriveSecret = options: let
+					script = pkgs.writeShellApplication {
+						name = "age-derive-secret-${options.name}";
+						runtimeInputs = options.inputs;
+						text = "secret=${options.secret}\n${options.script}";
+					};
+				in ''
+					echo "deriving secret '${options.name}' from '${options.secret}' to '${options.path}'"
+					${lib.getExe script} > ${options.path}
+				'';
+			in {
+				text = builtins.concatStringsSep "\n" (
+					["echo '[agenix] deriving secrets...'"]
+					++ (map deriveSecret (builtins.attrValues cfg.derivedSecrets))
+				);
+				deps = ["agenix"];
+			};
+		})
+		
+		(lib.mkIf (config.age.secrets != {} && !config.services.openssh.enable) {
 			# If openssh is not enabled, it won't generate its keys.
 			# However, they're still required for agenix to work.
 			system.activationScripts.generateSSHKeys = {
@@ -36,6 +88,6 @@
 			# agenix only sets identityPaths by default if openssh is enabled
 			# from agenix/modules/age.nix
 			age.identityPaths = with lib; map (e: e.path) (filter (e: e.type == "rsa" || e.type == "ed25519") config.services.openssh.hostKeys);
-		};
-	in lib.mkMerge [baseConfig disabledSshConfig];
+		})
+	];
 }
--- modules/email.nix
+++ modules/email.nix
@@ -25,19 +25,63 @@ in {
 			owner = "smtpd";
 		};
 		
+		age.secrets.opensmtpdPassword = {
+			file = ../secrets/opensmtpdPassword.age;
+		};
+		
+		age.derivedSecrets.opensmtpdCreds = {
+			secret = config.age.secrets.opensmtpdPassword.path;
+			inputs = with pkgs; [opensmtpd];
+			script = ''
+				# smtpctl requires setgid which is provided by the wrapper
+				encryptedPassword=$(${config.security.wrapperDir}/smtpctl encrypt < $secret)
+				echo "mailuser $encryptedPassword"
+			'';
+		};
+		
+		modules.acme = {
+			enable = true;
+			domain = cfg.domain;
+			users = ["smtpd"];
+		};
+		
+		# because opensmtpd requires the certificate to be owned by root LoadCredential is necessary
+		# https://github.com/OpenSMTPD/OpenSMTPD/issues/1142
+		# https://nixos.org/manual/nixos/stable/#module-security-acme-root-owned
+		security.acme.certs.${cfg.domain}.postRun = ''
+			systemctl restart opensmtpd
+		'';
+		
+		systemd.services.opensmtpd.requires = ["acme-finished-${cfg.domain}.target"];
+		systemd.services.opensmtpd.serviceConfig.LoadCredential = let
+			certDir = config.security.acme.certs.${cfg.domain}.directory;
+		in [
+			"cert.pem:${certDir}/cert.pem"
+			"key.pem:${certDir}/key.pem"
+		];
+		
 		services.opensmtpd = {
 			enable = true;
 			extraServerArgs = ["-P mda"];
 			serverConfiguration = let
 				senders = lib.concatMapStringsSep "," (sender: "\"${sender}@${cfg.domain}\"") cfg.senders;
 				filter-dkimsign = "${pkgs.opensmtpd-filter-dkimsign}/libexec/opensmtpd/filter-dkimsign";
+				creds = config.age.derivedSecrets.opensmtpdCreds.path;
 				rsaKey = config.age.secrets.dkimRsaKey.path;
+				credsDir = "/run/credentials/opensmtpd.service";
 			in unindent ''
+				table "creds" "${creds}"
 				table "senders" { ${senders} }
+				
+				pki "cert" cert "${credsDir}/cert.pem"
+				pki "cert" key "${credsDir}/key.pem"
+				
 				filter "dkim-sign-rsa" proc-exec "${filter-dkimsign} -a rsa-sha256 -d dav.dev -s rsa -k ${rsaKey}"
+				
 				action "send" relay helo "${cfg.domain}"
 				match mail-from <"senders"> for any action "send"
-				listen on lo port ${toString cfg.port} filter "dkim-sign-rsa"
+				
+				listen on lo port ${toString cfg.port} filter "dkim-sign-rsa" tls pki "cert" auth <"creds">
 			'';
 		};
 	};
